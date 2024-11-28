330df3b64d47625ec804eb5f1919cfec06dafe49
Asya Fedorova <79772856+AsyaSt@users.noreply.github.com>
2024-11-28 15:50:37 +0200

    colors to expenses

--- src/constants/transactions.js
+++ src/constants/transactions.js
@@ -20,4 +20,38 @@ export const AVAILABLE_INCOME_CATEGORIES = [{
   label: 'Investment',
   incomeColor: 'mint',
 },
+// {
+//   value: 'Health',
+//   label: 'Health',
+//   incomeColor: 'mint',
+// },
+// {
+//   value: 'Food' ,
+//   label: 'Food',
+//   incomeColor: 'yellow'
+// },
+// {
+//   value: 'Shopping',
+//   label: 'Shopping',
+//   incomeColor: 'blue',
+// },
+
+{ value: 'Food', label: 'Food', incomeColor: 'blue' },
+  { value: 'Closing', label: 'Closing', incomeColor: 'green' },
+  { value: 'Health', label: 'Health', incomeColor: 'red' },
+  { value: 'Housing', label: 'Housing', incomeColor: 'purple' },
+  { value: 'Car', label: 'Car', incomeColor: 'orange' },
+  { value: 'Transportation', label: 'Transportation', incomeColor: 'teal' },
+  { value: 'Phone', label: 'Phone', incomeColor: 'cyan' },
+  { value: 'Electronics', label: 'Electronics', incomeColor: 'pink' },
+  { value: 'Education', label: 'Education', incomeColor: 'lime' },
+  { value: 'Beauty', label: 'Beauty', incomeColor: 'brown' },
+  { value: 'Sports', label: 'Sports', incomeColor: 'yellow' },
+  { value: 'Restaurants', label: 'Restaurants', incomeColor: 'navy' },
+  { value: 'Travel', label: 'Travel', incomeColor: 'olive' },
+  { value: 'Pets', label: 'Pets', incomeColor: 'gold' },
+  { value: 'Repairs', label: 'Repairs', incomeColor: 'gray' },
+  { value: 'Donations', label: 'Donations', incomeColor: 'violet' },
+  { value: 'Gifts', label: 'Gifts', incomeColor: 'indigo' },
+  { value: 'Lottery', label: 'Lottery', incomeColor: 'coral' }
 ];
